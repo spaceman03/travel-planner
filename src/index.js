90eb98811388d18e6e91c75b358cd064b1dec27c
Chung Zhi Xuan <zhixuanchung03@gmail.com>
2024-02-05 23:57:59 +0800

    Bug fixes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import TravelPlan from './TravelPlan';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import TravelPlan from "./pages/TravelPlan.js";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-
 ReactDOM.render(
   <Router>
-    <Switch>
+    <Routes>
       <Route exact path="/" component={App} />
       <Route path="/travel-plan" component={TravelPlan} />
-    </Switch>
+    </Routes>
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
