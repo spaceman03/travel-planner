a7715f19ab358221e5cdfaba5fe9b29d6775153f
Brandon Jang <bjang002@e.ntu.edu.sg>
2024-02-05 23:47:35 +0800

    g

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import TravelPlan from './TravelPlan';
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+);
+
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/travel-plan" component={TravelPlan} />
+    </Switch>
+  </Router>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/TravelPlan.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TravelPlan = () => {
+  return (
+    <div>
+      <h1>Your Travel Plan Page</h1>
+      {/* Add content for your travel plan page */}
+    </div>
+  );
+};
+
+export default TravelPlan;
