1ea7291e964b515a3b221a0697333e801db71701
Chung Zhi Xuan <zhixuanchung03@gmail.com>
2024-02-06 01:49:33 +0800

    fix image problem

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import SelectPage from "./selectPage.jsx";
 import "./landingPage.css";
+import logo from "../logo.png";
 
 const LandingPage = () => {
   return (
     <Router>
       <div className="landingPage">
+        <img src={logo} alt="Landing page" />
         <button>
           <Link to="/selectPage">Generate your travel plan now!</Link>
         </button>
--- src/pages/selectPage.jsx
+++ src/pages/selectPage.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 const SelectPage = () => {
+  const pageStyle = {
+    background: "none",
+  };
   return (
-    <div>
+    <div style={pageStyle}>
       <h2>Select Your Preferences</h2>
       <p>Choose your preferred options for your travel plan:</p>
       {/* Add your content and form elements here */}
